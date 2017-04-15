2e0badcf1f7c2942cb27db700e27d2dcd5a34ac0
Chris Quinn <cquinn@fool.com>
2017-04-14 22:54:22 -0400

    more advanced block movement with corporate icons

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 
 $(document).ready(function() {
-	var blocks = $('.slide-in');
-	$("#toggle-blocks").on("click", function() {
-		blocks.toggleClass('show');
+	var phaseOnes = $('.phase1');
+	var phaseTwos = $('.phase2');
+	var prevButton = $("#prev-group");
+	var nextButton = $("#next-group");
+	var leftBlock = $("#left-block");
+	var rightBlock = $("#right-block");
+	var icons = $('#icons > svg');
+	var speed = 300;
+
+	prevButton.on("click", function() {
+		nextButton.prop("disabled", false);
+		prevButton.prop("disabled", true);
+		moveTheBlocks(false);
 	});
+	nextButton.on("click", function() {
+		prevButton.prop("disabled", false);
+		nextButton.prop("disabled", true);
+		moveTheBlocks(true);
+	});
+
+	function moveTheBlocks(toNext) {
+		var questionGroup = $('.question-group.current');
+		var midHeight = ((Math.random() * 40) + 20)+ "%";
+		var leftIcon, rightIcon;
+		var notThisIndex = -1;
+
+		function getRandomIcon() {
+			do {
+				randomIndex = Math.floor(Math.random() * icons.length);
+			} while (randomIndex === notThisIndex);
+			console.log(notThisIndex, randomIndex);
+			notThisIndex = randomIndex;
+			return icons.eq(randomIndex);
+		}
+
+		leftBlock.html(getRandomIcon());
+		rightBlock.html(getRandomIcon());
+
+		//phaseTwos.css("top", midHeight);
+
+		phaseOnes.toggleClass('show');
+		setTimeout(function() {
+			phaseTwos.toggleClass('show');
+		}, speed);
+		setTimeout(function() {
+			phaseTwos.toggleClass('show');
+			questionGroup.removeClass('current');
+			if (toNext) {
+				questionGroup.next().addClass('current');
+			}
+			else {
+				questionGroup.prev().addClass('current');
+			}
+		}, speed * 2.5);
+		setTimeout(function() {
+			phaseOnes.toggleClass('show');
+		}, speed * 3);
+	}
 });
